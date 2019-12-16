c3bdae39c5fffa377c21f9d8e9ad48e9f27ac4db
David Ferdinand <ferdinando_83@outlook.com>
2019-12-15 21:16:13 -0500

    Add websocket functionality
    Add websocket hook
    Modify App component to display data feed
    Update project version

--- src/components/App/AppView.js
+++ src/components/App/AppView.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useWebsocket} from "../../utilities/useWebsocket";
 
 function AppView() {
+  const [price, setPrice] = useState("");
+
+  const websocket = useWebsocket("wss://ws-feed.pro.coinbase.com");
+
+  useEffect(() => {
+    websocket.onmessage = (message) => {
+      const data = JSON.parse(message.data);
+      setPrice(data.price);
+    }
+  }, []);
+
   return (
-    <header>Trade Tool Header</header>
+    <header>{price}</header>
   )
 }
 
