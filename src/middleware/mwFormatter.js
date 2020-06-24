aad18008d34da3584e3966b43a16a5047754bd2a
David Ferdinand <ferdinando_83@outlook.com>
2020-06-23 19:59:43 -0500

    Update mwFormatter.js to format and prepare data for universal schema

--- src/middleware/mwFormatter.js
+++ src/middleware/mwFormatter.js
@@ -1,28 +1,23 @@
-import * as actions from "../modules/book"
+import * as actions from "../modules/book";
+import _ from "lodash";
 
 const mwFormatter = store => {
 
   const filterAction = (store, action) => {
     switch (action.message.type) {
       case "status":
-        const ids = action.message.products.map(product => product.id);
+        const products = _.assign({}, ...action.message.products.map(product => {
+          return actions.productSchema(product.id, product);
+        }));
 
-        store.dispatch(actions.statusReady(ids));
+        store.dispatch(actions.statusReady(products));
         break;
 
       case "ticker":
-        const product_data = {
-          data:
-            {
-              price: action.message.price,
-              side: action.message.side,
-              size: action.message.last_size,
-              best_bid: action.message.best_bid,
-              best_ask: action.message.best_ask
-            }
-        };
+        const id = action.message.product_id;
+        const data = action.message;
 
-        store.dispatch(actions.productUpdate(action.message.product_id, product_data));
+        store.dispatch(actions.productUpdate(id, actions.productSchema(id, data)));
         break;
 
       default:
