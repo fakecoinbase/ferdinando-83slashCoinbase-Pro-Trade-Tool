cdbe53a19812875d51860fa4d9b540531bd3771b
David Ferdinand <ferdinando_83@outlook.com>
2020-06-23 19:52:23 -0500

    Add mwAlgorithm to middleware index for default export

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
 import mwWebsocket from "./mwWebsocket";
 import mwFormatter from "./mwFormatter";
+import mwAlgorithm from "./mwAlgorithm";
 import thunk from "redux-thunk";
 
-export default [mwWebsocket, mwFormatter, thunk];
+export default [mwWebsocket, mwFormatter, mwAlgorithm, thunk];
